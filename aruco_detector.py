487dca043bcea4c786766d5304d25aeff41835b2
Mavis Murdock <mavis.murdock@tufts.edu>
2025-08-19 14:08:38 -0400

    add a function for returning the last known position of each tag

--- aruco_detector.py
+++ aruco_detector.py
@@ -1,47 +1,3 @@
-
-
-
-# class ArucoDetector:
-    
-#     def __init__(self):
-#         self.dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
-#         self.parameters = cv2.aruco.DetectorParameters()
-#         self.detector = cv2.aruco.ArucoDetector(self.dictionary, self.parameters)
-
-#         self.aruco_tag = Aruco()
-        
-#     async def visualize(self):
-#         # Connect to webcam
-#         self.cap = cv2.VideoCapture(0)
-
-#         while self.visualizing == True and self.cap.isOpened():
-#             ret, frame = self.cap.read()
-#             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-#             corners, ids, rejected = self.detector.detectMarkers(gray)
-#             # print("corners of aruco tag:", corners)
-#             if not len(corners) == 0:
-#                 self.aruco_tag.update_corners(corners)
-#                 print("detected")
-
-#             if ids is not None:
-#                 cv2.aruco.drawDetectedMarkers(frame, corners, ids)
-
-#             cv2.imshow('Detected markers', frame)
-
-#             if cv2.waitKey(1) & 0xFF == ord('q'):
-#                 break
-            
-#         self.cap.release()
-#         cv2.destroyAllWindows()
-        
-#     def begin_visualization(self):
-#         self.visualizing = True
-#         asyncio.run(self.visualize())
-        
-#     def end_visualization(self):
-#         self.visualizing = False
-
 import cv2
 from matplotlib import pyplot as plt
 from aruco_obj import Aruco
@@ -55,7 +11,7 @@ class ArucoDetector:
         self.detector = cv2.aruco.ArucoDetector(self.dictionary, self.parameters)
 
         self.aruco_tags = {}
-        # self.aruco_tag = Aruco()
+
         self.visualizing = False
         self.cap = None
         self._task = None  # asyncio task handle
@@ -112,3 +68,8 @@ class ArucoDetector:
 
     def get_tags(self):
         return self.aruco_tags
+    
+    def get_last_tag_corners(self):
+        return {k: v.get_last_corners() for k, v in self.aruco_tags.items()} 
+    
+    
